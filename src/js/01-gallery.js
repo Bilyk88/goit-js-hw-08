93639fdff4195cae7f8a06a71aaaaa04daa0c3ed
Iryna Bilyk <bilykirochka@gmail.com>
2023-11-02 20:03:23 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+
+
+
+galleryList.insertAdjacentHTML('beforeend', markup);
 
 console.log(galleryItems);
