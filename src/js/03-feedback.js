21d7f08896bcf721217e397e12377347839f361a
Iryna Bilyk <bilykirochka@gmail.com>
2023-11-13 22:47:28 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,10 @@
 
+// import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('input', onInput);
+
+function onInput() {
+    console.dir(feedbackForm.email.value);
+}
