b7a7d921a9f3d2bc28929937747445ace3c3e188
Iryna Bilyk <bilykirochka@gmail.com>
2023-11-13 22:04:40 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,31 +5,29 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const KEY_TIME = "videoplayer-current-time";
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.on('timeupdate', throttle(timeUpdate, 1000));
 
-player.on('timeupdate', throttle(function (data) {
+function timeUpdate(data) {
+    console.log(data);
     const currentTime = data.seconds;
     console.log(currentTime);
-    localStorage.setItem(KEY_TIME, JSON.stringify(data.seconds));
+    localStorage.setItem(KEY_TIME, JSON.stringify(currentTime));
+}
 
-}), 10000);
+// const savedTime = localStorage.getItem(KEY_TIME);
+// console.log(savedTime);
 
-const savedTime = localStorage.getItem(KEY_TIME);
-console.log(savedTime);
+// player.setCurrentTime(savedTime).then(function (seconds) {
+//     seconds = savedTime;
 
-player.setCurrentTime(savedTime).then(function (seconds) {
-    seconds = savedTime;
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
 
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
