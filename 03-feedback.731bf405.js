076c7f8b5bed971f864729555a826a1b241d1372
Bilyk88 <113465433+Bilyk88@users.noreply.github.com>
2023-11-17 19:36:10 +0000

    Deploying to gh-pages from @ Bilyk88/goit-js-hw-08@16e92335b64b949b4aafa679fa42fe85d9b33c1a 🚀

--- 03-feedback.731bf405.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(){console.dir(e.email.value)}));
-//# sourceMappingURL=03-feedback.731bf405.js.map
--- 03-feedback.909de2f8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");e.addEventListener("input",(function(){console.dir(e.email.value)}))}();
-//# sourceMappingURL=03-feedback.909de2f8.js.map
