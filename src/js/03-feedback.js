352c88c3bf41ce0df89dc17ac170ce337f116e11
Iryna Bilyk <bilykirochka@gmail.com>
2023-11-18 15:04:22 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,9 @@
 import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('[type="email"]');
+const textarea = document.querySelector('[name="message"]');
+
 const FORM_KEY = 'feedback-form-state';
 const data = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {};
 
@@ -10,29 +13,34 @@ fillForm();
 feedbackForm.addEventListener('input', throttle(onInput, 500));
 feedbackForm.addEventListener('submit', onSubmit);
 
-function onInput(event) {
+function onInput() {
 
-    data[event.target.name] = event.target.value;
+    data.email = email.value;
+    data.message = textarea.value;
 
     localStorage.setItem(FORM_KEY, JSON.stringify(data));
 
 }
 
 function onSubmit(event) {
+
     event.preventDefault();
     event.currentTarget.reset();
-    console.log(data);
-    localStorage.removeItem(FORM_KEY);
 
+    localStorage.removeItem(FORM_KEY);
+    
+    console.log(data);
+    
 }
 
 function fillForm() {
-    const savedMessage = localStorage.getItem(FORM_KEY); 
+    
+    const savedMessage = JSON.parse(localStorage.getItem(FORM_KEY)); 
 
     if (savedMessage) {
-        const { email, message } = JSON.parse(savedMessage);
 
-        feedbackForm.elements.email.value = email ?? '';
-        feedbackForm.elements.message.value = message ?? '';
+        email.value = savedMessage.email;
+        textarea.value = savedMessage.message;
+
     }
-}
+} 
